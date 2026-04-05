706f7060e71ce52b55a7ed54f3c8bbb74c5c9650
minhduy <MDuy10x203@gmail.com>
2026-04-05 19:44:08 +0700

    add method getAllStaffs

--- src/main/java/com/isums/userservice/infrastructures/abstracts/UserService.java
+++ src/main/java/com/isums/userservice/infrastructures/abstracts/UserService.java
@@ -1,9 +1,6 @@
 package com.isums.userservice.infrastructures.abstracts;
 
-import com.isums.userservice.domains.dtos.CreateTechnicalStaffRequest;
-import com.isums.userservice.domains.dtos.KeycloakCreateUserRequest;
-import com.isums.userservice.domains.dtos.UserDto;
-import com.isums.userservice.domains.dtos.UserProfileDto;
+import com.isums.userservice.domains.dtos.*;
 import com.isums.userservice.domains.events.DepositPaidEvent;
 
 import java.util.List;
@@ -26,4 +23,6 @@ public interface UserService {
     void activateIfNewUser(DepositPaidEvent event);
 
     UserDto createTechnicalStaff(CreateTechnicalStaffRequest req);
+
+    List<StaffDto> getAllStaff();
 }
